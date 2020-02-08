0dae7778e21681cebce88ed8fa9449229f1333ac
Tim54000 <tim54000@gmx.com>
2020-02-08 19:33:32 +0100

    Change in the method of importing remailers.
    === ADDITIONS ===
    - Add a config file containing the keys, email and names of the
    different remailers
    - Some minor changes
    
    === DELETIONS ===
    - All remailers' armored key
    
    === TODO ===
    - Make comments
    - Using structopt to make a real CLI program
    - Add the other output format (EML, Mailto, clear, ...)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::fs::File;
+use std::fs::{File, read};
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
 use failure::{err_msg, Fail, Fallible, ResultExt};
 use failure::Error as FError;
+use serde_derive::{Deserialize, Serialize};
 
 use crate::lib::{Cypherpunk, CypherpunkCore, PGPBackend};
 #[cfg(feature = "back-gpg")]
@@ -12,49 +13,53 @@ use crate::pgp::gpg::GPGBackend;
 mod lib;
 mod pgp;
 
+/// TODO:
+/// - Make comments
+/// - Make the structopt to make a real CLI program
+/// - Add the other output format (EML, Mailto, clear, ...)
+
+
 fn main() {
+    // To init a PGP Back, here for the GPG one.
     #[cfg(feature = "back-gpg")]
     fn init_pgp_back() -> impl PGPBackend {
         GPGBackend::new(None, false)
     }
-    let pgp_back = init_pgp_back();
-    let core = CypherpunkCore::new(Box::new(pgp_back));
-    let keys = load_keys();
-    core.import_keys(keys.unwrap()).map_err(print_errors);
-    let encrypted = core.encrypt_message(vec!["mixmaster@remailer.privacy.at".to_string(), "remailer@dizum.com".to_string()], "::\nAnon-To: test@domain.tld\n\nHey!".as_bytes().to_vec());
-    encrypted.map(|msg| -> Fallible<()> {
-        println!("Final:\n{}", String::from_utf8(msg)?);
+
+    // Load config and run all
+    load_config("./remailers.json").and_then(|config| {
+
+        // Init infra (the PGP backend)
+        let pgp_back = init_pgp_back();
+        // Init the domain (the CypherpunkCore)
+        let core = CypherpunkCore::new(Box::new(pgp_back));
+
+        // Retrieve remailers' keys from config
+        let keys: Vec<Vec<u8>> = config.remailers.iter().filter_map(|remailer| {
+            match remailer.is_enabled() {
+                true => remailer.into_key().ok(),
+                false => None,
+            }
+        }).collect();
+
+        // Import keys in the Cypherpunk Core
+        core.import_keys(keys)?;
+
+        // Encrypt the mail
+        let encrypted = core.encrypt_message(vec!["mixmaster@remailer.privacy.at".to_string(), "remailer@dizum.com".to_string()], "::\nAnon-To: test@domain.tld\n\nHey!".as_bytes().to_vec());
+
+        // Print the mail if itis build with success
+        encrypted.map(|msg| -> Fallible<()> {
+            println!("Final:\n{}", String::from_utf8(msg)?);
+            Ok(())
+        })?;
+
         Ok(())
     }).map_err(print_errors);
 }
 
-fn load_keys() -> Fallible<Vec<Vec<u8>>> {
-    println!("Let's import the keys...");
-    let mut keys: Vec<Vec<u8>> = Vec::new();
-
-    // Retrieve the keys path and check it
-    let keys_path = Path::new("./remailer-keys/");
-    if !keys_path.exists() || !keys_path.is_dir() { // Show error if the `remailer-keys` directory doesn't exist
-        return Err(err_msg("The `remailer-keys` directory doesn't exist!"));
-    }
-    let keys_dir = std::fs::read_dir(keys_path)?; // List the keys presents in the directory
-    for entry in keys_dir {
-        match entry {
-            Ok(entry) => {
-                if entry.path().is_dir() { // Ignoring dir
-                    println!("Entry `{}` ignored!", entry.path().to_string_lossy());
-                } else { // Importing "key" file using the given `import_key` function
-                    let mut key_file = File::open(entry.path())?;
-                    let mut buffer = Vec::new();
-                    key_file.read_to_end(&mut buffer);
-                    keys.push(buffer);
-                }
-            }
-            // Ignore invalid DirEntry
-            Err(err) => eprintln!("An error occurred on a dir entry, it'll be ignored: {:?}", err),
-        }
-    }
-    return Ok(keys);
+fn load_config<P: AsRef<Path>>(path: P) -> Fallible<RemailerConfig> {
+    Ok(serde_json::from_reader(File::open(path)?)?)
 }
 
 fn print_errors(err: FError) {
@@ -65,5 +70,30 @@ fn print_errors(err: FError) {
     });
     if let trace = err.backtrace() {
         eprintln!("\n{}", trace)
+    }
+}
+
+#[derive(Deserialize, Eq, PartialEq, Clone, Debug, Default)]
+struct RemailerConfig {
+    version: String,
+    author: String,
+    remailers: Vec<Remailer>,
+}
+
+#[derive(Deserialize, Eq, PartialEq, Clone, Debug, Default)]
+struct Remailer {
+    name: Vec<String>,
+    email: String,
+    enable: bool,
+    key: String,
+}
+
+impl Remailer {
+    fn is_enabled(&self) -> bool {
+        self.enable
+    }
+
+    fn into_key(&self) -> Fallible<Vec<u8>> {
+        Ok(base64::decode(self.key.split_at(7).1).context(format!("Can't decode the base64-encoded key `{}`!", self.name[0]))?)
     }
 }
